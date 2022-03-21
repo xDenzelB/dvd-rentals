f4039de6972c26c25bc21a31b5ec9754860ead41
Denzel <dbartolaba123@gmail.com>
2022-03-21 14:49:21 -0700

    got my 6th and 5th test passing

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,20 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+SELECT 
+film.title
+FROM 
+film
+LEFT JOIN 
+inventory 
+ON 
+inventory.film_id = film.film_id
+LEFT JOIN 
+rental 
+ON 
+rental.inventory_id = inventory.inventory_id
+LEFT JOIN 
+customer 
+ON 
+customer.customer_id = rental.customer_id
+WHERE 
+customer.first_name = 'Roberta';
